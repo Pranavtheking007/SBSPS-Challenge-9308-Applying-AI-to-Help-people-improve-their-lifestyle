2ecd8e0b6bf2bfce12df21bb21b68c1a93ae6e24
Pranav <pranav.ms@somaiya.edu>
2022-10-12 12:38:35 +0530

    Added new files for categories

---
+++ health_tips.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
